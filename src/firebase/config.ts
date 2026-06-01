38c13d796790bf58a435361a7e99c936a83c3a34
shrqbabu <techloot1100@gmail.com>
2026-06-02 01:44:01 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "@/firebase-applet-config.json";
+
 
 // Initialize Firebase App
 const app = initializeApp(firebaseConfig);
