2dcb8245f56d97990b611a76765e1e87639f968c
shrqbabu <techloot1100@gmail.com>
2026-06-02 00:32:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import MatchDetailsPage from "./pages/MatchDetailsPage";
+import {HomePage} from "./pages/HomePage";
+import {MatchDetailsPage} from "./pages/MatchDetailsPage";
 
 export default function App() {
   return (
